849b65f49601feaf2bb7755fd2e6c168d89e5f79
yugyeogn <149449426+yugyeogn@users.noreply.github.com>
2023-11-12 14:58:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("typing-text");
+  
+  const textToType = " 유경 \n홈페이지  ";
+  const typingSpeed = 400; // 글자당 밀리초
+
+  function typeText(text, index) {
+      if (index < text.length) {
+          textElement.innerText += text.charAt(index);
+          setTimeout(function () {
+              typeText(text, index + 1);
+          }, typingSpeed);
+      }
+  }
+
+  typeText(textToType, 0);
+});
+
+var homePage = document.getElementById("home");
+var coursesPage = document.getElementById("courses");
+var photoPage = document.getElementById("photo");
+
+var menuLinks = document.querySelectorAll(".menu a");
+
+for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener("click", function() {
+        this.classList.add("active");
+
+        var pageId = this.getAttribute("href");
+        var page = document.getElementById(pageId);
+
+        if (pageId === "#home") {
+            homePage.innerHTML = page.innerHTML;
+        } else if (pageId === "#courses") {
+            coursesPage.innerHTML = page.innerHTML;
+        } else {
+            photoPage.innerHTML = page.innerHTML;
+        }
+    });
+}
+
+ 
---
+++ todo.js
@@ -0,0 +1,68 @@
+const containerBox = document.querySelector("#containerBox"),
+    contents = containerBox.querySelector("#contents"),
+    list = contents.querySelector("#list"),
+    inputList = document.querySelector(".inputlist"),
+    listForm = inputList.querySelector("#listForm"),
+    input = listForm.querySelector("input");
+const list_LS = "toDos";
+
+let toDos = [];
+
+function saveList(){
+    localStorage.setItem(list_LS, JSON.stringify(toDos));
+}
+
+function deleteList(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    list.removeChild(li);
+    const cleanList = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanList;
+    saveList();
+}
+
+function showingList(text){
+    const li = document.createElement("li");
+    const delbtn = document.createElement("button");
+    const span = document.createElement("span");
+    const listNum = toDos.length+1;
+    delbtn.innerText = "(지우기)";
+    delbtn.addEventListener("click", deleteList);
+    span.innerText = text;
+    list.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(delbtn);
+    li.id = listNum;
+    const toDoObj = {
+        text: text,
+        id: listNum
+    };
+    toDos.push(toDoObj);
+    saveList();
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    showingList(currentValue);
+    input.value = "";
+}
+
+function loadList(){
+    const loadedList = localStorage.getItem(list_LS);
+    if(loadedList !== null){
+        const parsedData = JSON.parse(loadedList);
+        parsedData.forEach(function(toDo){
+            showingList(toDo.text);
+        });
+    }
+}
+
+function init(){
+    loadList();
+    listForm.addEventListener("submit", handleSubmit);
+}
+
+init();
