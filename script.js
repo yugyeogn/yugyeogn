d569ceccd5140695826b4424b765504dc04c974a
yugyeogn <149449426+yugyeogn@users.noreply.github.com>
2023-11-20 16:16:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const textElement = document.getElementById("typing-text");
-  
-  const textToType = " 스케줄 관리\n 홈페이지 ";
+  const textToType = " Schedule \n Management   ";
+ 
   const typingSpeed = 400; // 글자당 밀리초
 
   function typeText(text, index) {
